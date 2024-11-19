02ed3e64821eccceb5dd77550bf61df4ad12e32c
Redcloak863 <redcloak@protonmail.com>
2024-11-19 11:28:20 -0600

    Update comments

--- Splitter.py
+++ Splitter.py
@@ -1,14 +1,14 @@
-import random
-import sys
+import random #You'll be picking a lucky winner at random
+import sys #To quit the program on an invalid input
 
 def split(f, b, l): #Takes the friends dictionary, the bill, and the lucky flag
-        num = f.__len__()
-        if l:
-            num -= 1
+        num = f.__len__() #Returns the nunber of dictionary entries
+        if l: #Lucky as a boolean
+            num -= 1 #Split n-1 ways; otherwise, n ways
         split = round((b / num), 2)
         for each in f:
             f[each] = split
-        return f
+        return f #The dictionary with the split added. Note that the lucky winner is still getting charged at this point.
 
 def luckyOne(l):            #Takes a dictionary as an argument
     g = list(l.keys())      #Creat a list of the keys
@@ -19,9 +19,9 @@ if __name__ == '__main__':
 
     try:
         friend_count = int(input('Enter the number of friends joining (including you): '))
-    except ValueError:
+    except ValueError: #Fix anything that wasn't an integer
         friend_count = 0 #Well, if that's how you're going to be...
-    if friend_count > 0:
+    if friend_count > 0: #Must be positive and non-zero
         friends = {}
         for each in range(friend_count):
             friend = input('Enter the name of every friend (including you), each on a new line: ')
@@ -29,11 +29,11 @@ if __name__ == '__main__':
         bill = int(input('Enter the total bill value: '))
     else:
         print('No one is joining for the party')
-        sys.exit(0)
+        sys.exit(0) #Quit because the input wasn't valid
 
     lucky = input('Do you want to use the "Who is lucky?" feature? Write Yes/No: ')
     if lucky == 'Yes':
-        lucky = True
+        lucky = True #We're done using lucky as a string, let's use it for our boolean
         winner = luckyOne(friends)
         print(f'{winner} is the lucky one!')
     else:
@@ -41,5 +41,5 @@ if __name__ == '__main__':
         print('No one is going to be lucky')
     friends = split(friends, bill, lucky)
     if lucky: #lucky is now a boolean...
-        friends[winner] = 0.00
+        friends[winner] = 0.00 #Set the lucky winner's share to 0
     print(friends)
